2fd440a8014c746528599dbabd56bd6269227f88
Eko Saputra <ekosaputra.az88@gmail.com>
2022-10-24 21:37:08 +0700

    herii

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>TES WEBSITE INABATUL QURAN kota dumai tes</h1>
+      <h1>TES WEBSITE INABATUL QURAN kota dumai tes heri</h1>
     </div>
   );
 }
